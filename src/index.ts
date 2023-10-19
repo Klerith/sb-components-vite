4f622b745b42afcc975925e4a270b95c151c284d
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-19 10:33:57 -0400

    genera archivos de definición

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+
+
+export { Button } from './stories/Button';
+export { Header } from './stories/Header';
+export { MyLabel } from './components/MyLabel';
+export { Page } from './stories/Page';
+
+
+
